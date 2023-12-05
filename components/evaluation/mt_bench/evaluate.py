a77754127c19b9c7789f5012b5f1690412f269e3
Lukas Wutschitz <luwutsch@microsoft.com>
2023-12-05 15:37:12 +0000

    Download ebench data

--- components/evaluation/mt_bench/evaluate.py
+++ components/evaluation/mt_bench/evaluate.py
@@ -1,46 +1,49 @@
+import torch
 from shutil import copy2
 from pathlib import Path
 from subprocess import check_call
 from pydantic_cli import run_and_exit
 from pydantic import BaseModel
+from urllib.request import urlretrieve
 
-from fastchatllm_judge import gen_model_answer, gen_judgement, show_result
+from fastchat.llm_judge import gen_model_answer, gen_judgment, show_result
 
 
 class Arguments(BaseModel):
-    model: str
-    model_args: str = ""
-    tasks: str = None
-    provide_description: bool = False
-    num_fewshot: int = 0
-    batch_size: str = None
-    max_batch_size: int = None
-    device: str = None
-    output_path: Path = None
-    limit: float = None
-    data_sampling: float = None
-    no_cache: bool = False
-    decontamination_ngrams_path: str = None
-    description_dict_path: Path = None
-    check_integrity: bool = False
-    write_out: bool = False
-    output_base_path: str = None
+    model: Path
+    num_gpus_per_model: int
+    num_concurrent_api_calls: int
+    results: Path
+    judgements: Path
+    answers: Path
+    num_total_gpus: int = torch.cuda.device_count()
 
 
 def main(args: Arguments) -> int:
+    # Download data
+    mt_bench_data_path = Path("data")/"mt_bench"
+    mt_bench_data_path.mkdir(parents=True, exist_ok=True)
+    urlretrieve("https://raw.githubusercontent.com/lm-sys/FastChat/main/fastchat/llm_judge/data/mt_bench/question.jsonl", mt_bench_data_path/"question.jsonl")
+
+    model_id = "model"
+
     gen_answers_script = gen_model_answer.__file__
-    model_id = model_path.name
-    check_call(["python", gen_answers_script, "--model-path", str(model_path), "--model-id", model_id,
-                "--num-gpus-total", num_gpus_total, "--num-gpus-per-model", num_gpus_per_model])
+    check_call([
+        "python", gen_answers_script, "--model-path", str(args.model), "--model-id", model_id,
+        "--num-gpus-total", str(args.num_total_gpus), "--num-gpus-per-model", str(args.num_gpus_per_model)
+    ])
+    copy2(Path("data")/"mt_bench"/"model_answer"/model_id+".jsonl", args.answers)
 
-    gen_judgement_script = gen_judgement.__file__
-    check_call(["python", gen_judgement_script, "--model-id", model_id, "--parallel", num_concurrent_api_calls])
-
-    show_result_script = show_result.__file__
-    check_call(["python", show_result_script, "--model-id", model_id, "--parallel", num_concurrent_api_calls])
+    gen_judgment_script = gen_judgment.__file__
+    check_call(["python", gen_judgment_script, "--model-id", model_id, "--parallel", args.num_concurrent_api_calls])
+    copy2(Path("data")/"mt_bench"/"model_judgement"/model_id+".jsonl", args.judgements)
 
     return 0
 
 
+def exception_handler(ex):
+    raise RuntimeError("Error in evaluation") from ex
+
+
 if __name__ == "__main__":
-    run_and_exit(main, Arguments)
+    run_and_exit(Arguments, main, exception_handler=exception_handler)
