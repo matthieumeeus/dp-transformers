e5112228665fbf31ad9b347bb8fdb4177d76a0d9
Lukas Wutschitz <luwutsch@microsoft.com>
2023-12-06 14:08:00 +0000

    Pass through OAI API keys

--- components/evaluation/mt_bench/evaluate.py
+++ components/evaluation/mt_bench/evaluate.py
@@ -1,11 +1,15 @@
 import torch
 import fastchat
+import os
+
 from shutil import copy2
 from pathlib import Path
-from subprocess import check_call
+from subprocess import check_call, Popen, PIPE, CalledProcessError
 from pydantic_cli import run_and_exit
 from pydantic import BaseModel
 from urllib.request import urlretrieve
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 from fastchat.llm_judge import gen_model_answer, gen_judgment
 
@@ -17,6 +21,11 @@ class Arguments(BaseModel):
     results: Path
     judgements: Path
     answers: Path
+    openai_api_base: str = None
+    openai_api_key_secret_name: str = None
+    openai_api_type: str = None
+    openai_api_version: str = None
+    vault_uri: str = None
     num_total_gpus: int = torch.cuda.device_count()
 
 
@@ -40,9 +49,23 @@ def download_llm_judge_data(fschat_version: str, download_path: Path):
 
 
 def main(args: Arguments) -> int:
+    cwd = Path.cwd()
+
+    aoai_env = dict()
+    if args.openai_api_base is not None:
+        aoai_env["OPENAI_API_BASE"] = args.openai_api_base
+    if args.openai_api_key_vault_name is not None and args.vault_uri is not None:
+        credential = DefaultAzureCredential()
+        secret_client = SecretClient(credential=credential, vault_url=args.vault_uri)
+        aoai_env["OPENAI_API_KEY"] = secret_client.get_secret(args.openai_api_key_secret_name)
+    if args.openai_api_type is not None:
+        aoai_env["OPENAI_API_TYPE"] = args.openai_api_type
+    if args.openai_api_version is not None:
+        aoai_env["OPENAI_API_VERSION"] = args.openai_api_version
+
     fschat_version = get_fschat_version()
     print(f"Found FastChat version {fschat_version}")
-    download_llm_judge_data(fschat_version=fschat_version, download_path=Path("."))
+    download_llm_judge_data(fschat_version=fschat_version, download_path=cwd)
 
     model_id = "model"
 
@@ -53,17 +76,21 @@ def main(args: Arguments) -> int:
     ]
     print(" ".join(gen_answers_call))
     check_call(gen_answers_call)
-    copy2(Path("data")/"mt_bench"/"model_answer"/(model_id+".jsonl"), args.answers)
+    copy2(cwd/"data"/"mt_bench"/"model_answer"/(model_id+".jsonl"), args.answers)
 
-    # Script asks for enter to continue, simulate this input here
-    stdin_to_judgment_script = b"\n"
     gen_judgment_script = gen_judgment.__file__
     gen_judgment_call = [
         "python", gen_judgment_script, "--model-list", str(model_id), "--parallel", str(args.num_concurrent_api_calls)
     ]
     print(" ".join(gen_judgment_call))
-    check_call(gen_judgment_call, input=stdin_to_judgment_script)
-    copy2(Path("data")/"mt_bench"/"model_judgement"/(model_id+".jsonl"), args.judgements)
+    env = os.environ.copy()
+    env.update(aoai_env)
+    proc = Popen(gen_judgment_call, stdin=PIPE, env=env)
+    # Script asks for 'enter' to continue, simulate this input here
+    proc.communicate(input=b"\n")
+    if proc.returncode != 0:
+        raise CalledProcessError(proc.returncode, gen_judgment_script)
+    copy2(cwd/"data"/"mt_bench"/"model_judgement"/(model_id+".jsonl"), args.judgements)
 
     return 0
 
