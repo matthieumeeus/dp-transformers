b7d5ad28e4a3628bba58e8ec433227dc51341d4a
Lukas Wutschitz <luwutsch@microsoft.com>
2023-12-19 17:09:29 +0000

    Template prompt generation

--- research/auditing-synthetic-text-generation/components/preprocess_for_synthesizer/preprocess.py
+++ research/auditing-synthetic-text-generation/components/preprocess_for_synthesizer/preprocess.py
@@ -9,28 +9,53 @@ from functools import partial
 class Arguments(BaseModel):
     dataset: Path
     train_data: Path
+    templated_prompt: str
+    label_name: str
+    text_name: str
 
 
 def convert_classification_record_to_synthesizer_record(
-        record: Dict[str, Any], label_int2str: Callable[[int], str]
+        record: Dict[str, Any], text_name: str, templated_prompt: str, label_name: str, label_int2str: Callable[[int], str]
     ) -> Dict[str, Any]:
-    label_str = label_int2str(record["label"])
+    """
+    Converts a classification record to a synthesizer record by replacing the label name in the templated prompt.
+
+    Args:
+        record (Dict[str, Any]): The classification record containing the text and label information.
+        text_name (str): The name of the text field in the record.
+        templated_prompt (str): The templated prompt with placeholders for the label name.
+                                The placeholder should be in the form of '{label_name}'.
+        label_name (str): The name of the label field in the record.
+        label_int2str (Callable[[int], str]): A function to convert the label integer to its corresponding string representation.
+
+    Returns:
+        Dict[str, Any]: The synthesizer record with the replaced prompt and the original text.
+
+    Raises:
+        ValueError: If the label name is not found in the templated prompt.
+    """
+    label_str = label_int2str(record[label_name])
+    if f"{{{label_name}}}" not in templated_prompt:
+        raise ValueError(f"Label name '{{{label_name}}}' not found in templated prompt: {templated_prompt}")
+    prompt = templated_prompt.replace(f"{{{label_name}}}", label_str)
     return {
-        "prompt": f"A sentence with a {label_str} sentiment: ",
-        "completion": record["sentence"]
+        "prompt": prompt,
+        "completion": record[text_name],
     }
 
 
 def main(args: Arguments) -> int:
-    dataset = load_from_disk(args.dataset)
+    dataset = load_from_disk(args.dataset, keep_in_memory=True)
 
     dataset = dataset.map(
         partial(
             convert_classification_record_to_synthesizer_record,
-            label_int2str=dataset.features["label"].int2str
+            label_int2str=dataset.features["label"].int2str,
+            text_name=args.text_name,
+            templated_prompt=args.templated_prompt,
+            label_name=args.label_name
         ),
-        remove_columns=dataset.column_names,
-        num_proc=8
+        remove_columns=dataset.column_names
     )
 
     dataset.to_json(args.train_data)
