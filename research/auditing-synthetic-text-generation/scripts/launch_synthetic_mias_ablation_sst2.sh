008f5d9b688c87ad23c8a7e6b939a6ec8ee5b507
matthieumeeus <t-mmeeus@microsoft.com>
2024-08-05 05:23:16 +0000

    Ablation experiment

---
+++ research/auditing-synthetic-text-generation/scripts/launch_synthetic_mias_ablation_sst2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the array of mia_method values
+mia_methods=("jaccard_1" "jaccard_5" "jaccard_10" "jaccard_25"
+             "embedding_1" "embedding_5" "embedding_10" "embedding_25"
+             "ngram_1" "ngram_2" "ngram_3" "ngram_4")
+
+# Loop through each mia_method value and launch the job
+for method in "${mia_methods[@]}"; do
+    echo "Launching the job with mia_method=${method}"
+    python estimate_privacy_synthetic.py --config-name synthetic_sst2_syntheticcanary_uniformlabel +submit=True ++shared_inference_parameters.mia_method=${method} 
+done
+
+echo "All jobs launched completed."
