be0ddb4c1a3a0f5a58513f71f0bc36aff5d11571
Lukas Wutschitz <luwutsch@microsoft.com>
2024-09-02 10:55:23 +0000

    Increase ppl max

--- research/auditing-synthetic-text-generation/scripts/launch_synthetic_prefix_sst2.sh
+++ research/auditing-synthetic-text-generation/scripts/launch_synthetic_prefix_sst2.sh
@@ -4,7 +4,7 @@
 min_ppls=(9.0 28.46 90.0 284.60 900.0 2846.05 9000.0 28460.50 90000.0)
 max_ppls=(11.0 34.79 110.0 347.85 1100.0 3478.51 11000.0 34785.05 110000.0)
 min_temps=(0.8 0.8 0.8 0.8 0.8 0.8 0.8 0.8 0.8)
-max_temps=(1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0)
+max_temps=(1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.4 1.4)
 
 # Loop through each config and its corresponding additional parameter
 for i in "${!min_ppls[@]}"; do
